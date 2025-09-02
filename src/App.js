e534a2c6fc3b9517feec5a2ec481fbba254dc38d
Smanga-28 <smangalisonkanyezi@gmail.com>
2025-09-02 12:57:59 +0200

    Changes on the home image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./components/App.css";
 import React from 'react';
 import { useContext, useEffect } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="home_container">
         <img
           className="home_image"
-          src="https://m.media-amazon.com/images/I/61tUYouaCJL._SX3000_.jpg"
+          src="https://m.media-amazon.com/images/G/53/RBSxFELA/2025/Abigail/Home-and-Kitchen/Home__Kitchen_DT.gif" 
           alt="Hero img"
         />
         <Products />
